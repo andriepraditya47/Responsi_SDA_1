cebcace66fde11df45e6728fbb6790d085754fb5
andriepraditya47 <andriepradityanugroho47@gmail.com>
2026-05-10 19:08:46 +0700

    Add BST and Node class

--- C_PSDA01_RESPONSI1_L0125003_AndriePradityaNugroho.java
+++ C_PSDA01_RESPONSI1_L0125003_AndriePradityaNugroho.java
@@ -1,5 +1,73 @@
 import java.util.*;
 
+class Node {
+    int key;
+    Node left, right;
+
+    public Node(int item) {
+        key = item;
+        left = right = null;
+    }
+}
+
+class BinarySearchTree {
+    Node root;
+
+    BinarySearchTree() {
+        root = null;
+    }
+
+    // SEARCH
+    Node search(Node root, int key) {
+        if (root == null || root.key == key)
+            return root;
+
+        if (key < root.key)
+            return search(root.left, key);
+
+        return search(root.right);
+    }
+
+    // INSERT
+    Node insert(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+
+        if (key < root.key)
+            root.left = insert(root.left, key);
+        else
+            root.right = insert(root.right, key);
+
+        return root;
+    }
+
+    // DELETE
+    Node deleteNode(Node root, int key) {
+        if (root == null)
+            return root;
+
+        if (key < root.key)
+            root.left = deleteNode(root.left, key);
+        else if (key > root.key)
+            root.right = deleteNode(root.right, key);
+        else {
+            if (root.left == null)
+                return root.right;
+            else if (root.right == null)
+                return root.left;
+
+            Node temp = minValueNode(root.right);
+            root.key = temp.key;
+            root.right = deleteNode(root.right, temp.key);
+        }
+
+        return root;
+    }
+}
+
+
 public class C_PSDA01_RESPONSI1_L0125003_AndriePradityaNugroho {
   public static void main(String[] args){
     System.out.println("Hello World");
