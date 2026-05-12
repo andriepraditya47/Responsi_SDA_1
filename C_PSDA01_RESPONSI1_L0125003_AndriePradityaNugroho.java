3e21537c7d979e7d67f2f4b21c105be5cec3a622
andriepraditya47 <andriepradityanugroho47@gmail.com>
2026-05-13 00:31:12 +0700

    Add history feature and small changes
    History: print 10 recent logs along with the timestamp

--- C_PSDA01_RESPONSI1_L0125003_AndriePradityaNugroho.java
+++ C_PSDA01_RESPONSI1_L0125003_AndriePradityaNugroho.java
@@ -1,10 +1,13 @@
-/* 
-Contacts book oleh Andrie Praditya Nugroho L0125003, dapat diakses di 
+/*
+Contacts book oleh Andrie Praditya Nugroho L0125003, dapat diakses di
 https://github.com/andriepraditya47/Responsi_SDA_1
 */
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 class Node {
+
     String name;
     Node left, right;
 
@@ -15,6 +18,7 @@ class Node {
 }
 
 class BinarySearchTree {
+
     Node root;
 
     BinarySearchTree() {
@@ -28,21 +32,19 @@ class BinarySearchTree {
             return root;
         }
 
-        if (name.compareTo(root.name) < 0) // Comparing the value of the strings by each char
-            root.left = insert(root.left, name);
-        else
-            root.right = insert(root.right, name);
+        if (
+            name.compareTo(root.name) < 0 // Comparing the value of the strings by each char
+        ) root.left = insert(root.left, name);
+        else root.right = insert(root.right, name);
 
         return root;
     }
-    
+
     // SEARCH
     Node search(Node root, String name) {
-        if (root == null || name.equals(root.name))
-            return root;
+        if (root == null || name.equals(root.name)) return root;
 
-        if (name.compareTo(root.name) < 0)
-            return search(root.left, name);
+        if (name.compareTo(root.name) < 0) return search(root.left, name);
 
         return search(root.right, name);
     }
@@ -50,10 +52,10 @@ class BinarySearchTree {
     // Searching the node with the lowest value
     Node minValueNode(Node node) {
         Node current = node;
-        while (current.left != null)
-            current = current.left;
+        while (current.left != null) current = current.left;
         return current;
     }
+
     // Print all node in-order
     void printlist(Node root) {
         if (root != null) {
@@ -62,32 +64,33 @@ class BinarySearchTree {
             printlist(root.right);
         }
     }
-    
+
     // Print all node in-order with prefix
     void matchinglist(Node root, String keyword) {
-      if (root != null) {
-          matchinglist(root.left, keyword);
-          if (root.name.toLowerCase().contains(keyword.toLowerCase())){
-            System.out.println("- " + root.name);
-          }
-          matchinglist(root.right, keyword);
+        if (root != null) {
+            matchinglist(root.left, keyword);
+            if (root.name.toLowerCase().contains(keyword.toLowerCase())) {
+                System.out.println("- " + root.name);
+            }
+            matchinglist(root.right, keyword);
         }
     }
 
     // DELETE
     Node deleteNode(Node root, String name) {
-        if (root == null)
-            return root;
+        if (root == null) return root;
 
-        if (name.compareTo(root.name) < 0)
-            root.left = deleteNode(root.left, name);
-        else if (name.compareTo(root.name) > 0)
-            root.right = deleteNode(root.right, name);
+        if (name.compareTo(root.name) < 0) root.left = deleteNode(
+            root.left,
+            name
+        );
+        else if (name.compareTo(root.name) > 0) root.right = deleteNode(
+            root.right,
+            name
+        );
         else {
-            if (root.left == null)
-                return root.right;
-            else if (root.right == null)
-                return root.left;
+            if (root.left == null) return root.right;
+            else if (root.right == null) return root.left;
 
             Node temp = minValueNode(root.right);
             root.name = temp.name;
@@ -98,97 +101,148 @@ class BinarySearchTree {
     }
 }
 
-
 public class C_PSDA01_RESPONSI1_L0125003_AndriePradityaNugroho {
-  public static void main(String[] args){
-    Scanner userInput = new Scanner(System.in);
-    HashMap<String, String> map = new HashMap<>();
-    BinarySearchTree bst = new BinarySearchTree();
-   
-    System.out.println("####### CONTACTS BOOK #######");
-    
-    while(true){
-    System.out.println("\nSaved contacts:");
-    if(bst.root == null){
-      System.out.println("No contacts saved yet.");
-    }
-    else{
-          bst.printlist(bst.root);
-    }
-      System.out.println("\ninput command and name:");
-      String input = userInput.nextLine();
-      
-      if (input.equals("EXIT")){
-        return;
-      }
-      else if (input.equals("HELP")){
-          System.out.println("ADD: Input a new contact if not exist yet.");
-          System.out.println("DELETE: Delete an existing contact with given name.");
-          System.out.println("UPDATE: Update the phone number of specified contact.");
-          System.out.println("GET: Print the contact's name with the phone number.");
-          System.out.println("SEARCH: Print a list of contact's name with the specified keyword.");
-          System.out.println("EXIT: Quit the program.");
-          System.out.println("HELP: List all commands");
-      }
 
-      else{
-        String[] parts = input.split(" ", 2);
-        
-        if (parts.length < 2){
-        System.out.println("Please input contact's name.\n");
-        continue;
-      }
-        Node contactExists = bst.search(bst.root, parts[1]);
-      
-        switch (parts[0].toUpperCase()){
-          case "ADD":
-            if (contactExists != null){
-              System.out.println("Contact already exists.");
-              break;
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        HashMap<String, String> map = new HashMap<>();
+        BinarySearchTree bst = new BinarySearchTree();
+        LinkedList<String> log = new LinkedList<>();
+        DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+        System.out.println("####### CONTACTS BOOK #######");
+
+        while (true) {
+            System.out.println("\nSaved contacts:");
+            if (bst.root == null) {
+                System.out.println("No contacts saved yet.");
+            } else {
+                bst.printlist(bst.root);
             }
-            System.out.println("Input phone numbers:");
-            String phone = userInput.nextLine();
-            bst.root = bst.insert(bst.root, parts[1]);
-            map.put(parts[1], phone);
-            break;
+            System.out.println("\ninput command and name:");
+            String input = userInput.nextLine();
 
-          case "DELETE":
-            if (contactExists == null){
-            System.out.println("Contact does not exist.");
-            break;
+            if (input.equals("EXIT")) {
+                userInput.close();
+                return;
+            } else if (input.equals("HELP")) {
+                System.out.println(
+                    "ADD: Input a new contact if not exist yet."
+                );
+                System.out.println(
+                    "DELETE: Delete an existing contact with given name."
+                );
+                System.out.println(
+                    "UPDATE: Update the phone number of specified contact."
+                );
+                System.out.println(
+                    "GET: Print the contact's name with the phone number."
+                );
+                System.out.println(
+                    "SEARCH: Print a list of contact's name with the specified keyword."
+                );
+                System.out.println("EXIT: Quit the program.");
+                System.out.println("HISTORY: Prints all logs made.");
+                System.out.println("HELP: List all commands");
+            } else if (input.equals("HISTORY")) {
+                if (log.isEmpty()) {
+                    System.out.println("No logs has been recorded.");
+                }
+                for (String entry : log) {
+                    System.out.println(entry);
+                }
+            } else {
+                String[] parts = input.split(" ", 2);
+
+                if (parts.length < 2) {
+                    System.out.println("Please input contact's name.\n");
+                    continue;
+                }
+                Node contactExists = bst.search(bst.root, parts[1]);
+
+                LocalDateTime now = LocalDateTime.now();
+                String time = timeFormat.format(now);
+                switch (parts[0].toUpperCase()) {
+                    case "ADD":
+                        if (contactExists != null) {
+                            System.out.println("Contact already exists.");
+                            break;
+                        }
+                        System.out.println("Input phone numbers:");
+                        String phone = userInput.nextLine();
+                        bst.root = bst.insert(bst.root, parts[1]);
+                        map.put(parts[1], phone);
+
+                        log.addLast(
+                            "[" +
+                                time +
+                                "] " +
+                                "ADD: " +
+                                parts[1] +
+                                ", " +
+                                phone
+                        );
+
+                        System.out.println("Contact successfully added.");
+                        break;
+                    case "DELETE":
+                        if (contactExists == null) {
+                            System.out.println("Contact does not exist.");
+                            break;
+                        }
+                        map.remove(parts[1]);
+                        bst.root = bst.deleteNode(bst.root, parts[1]);
+                        log.addLast("[" + time + "] " + "DELETE: " + parts[1]);
+
+                        System.out.println("Contact successfully deleted.");
+
+                        break;
+                    case "UPDATE":
+                        if (contactExists == null) {
+                            System.out.println("Contact does not exist.");
+                            break;
+                        }
+                        System.out.println("Input phone number:");
+                        String newPhone = userInput.nextLine();
+                        map.put(parts[1], newPhone);
+
+                        log.addLast(
+                            "[" +
+                                time +
+                                "] " +
+                                "UPDATE: " +
+                                parts[1] +
+                                ", " +
+                                newPhone
+                        );
+
+                        System.out.println("Contact successfully updated.");
+
+                        break;
+                    case "GET":
+                        if (contactExists == null) {
+                            System.out.println("Contact does not exist.");
+                            break;
+                        }
+                        System.out.println(
+                            "\nName: " +
+                                parts[1] +
+                                "\nPhone number: " +
+                                map.get(parts[1])
+                        );
+                        break;
+                    case "SEARCH":
+                        System.out.println("\nSearch results: ");
+                        bst.matchinglist(bst.root, parts[1]);
+                        break;
+                    default:
+                        System.out.println("Please input valid command.");
+                        break;
+                }
+                if (log.size() > 10) {
+                    log.removeFirst();
+                }
             }
-            map.remove(parts[1]);
-            bst.root = bst.deleteNode(bst.root, parts[1]);
-            System.out.println("Contact successfully deleted.");
-          break;
-
-          case "UPDATE":
-            if (contactExists == null){
-            System.out.println("Contact does not exist.");
-            break;
-          }
-            System.out.println("Input phone number:");
-            String newPhone = userInput.nextLine();
-            map.put(parts[1], newPhone);
-            break;
-
-          case "GET":
-            if(contactExists == null){
-            System.out.println("Contact does not exist.");
-            break;
-            }
-            System.out.println("\nName: " + parts[1] + "\nPhone number: " + map.get(parts[1]));
-            break;
-          case "SEARCH":
-            System.out.println("\nSearch results: ");
-            bst.matchinglist(bst.root,parts[1]);
-            break;
-          default:
-            System.out.println("Please input valid command.");
-            break;
         }
-      }
-      
     }
-  }
 }
